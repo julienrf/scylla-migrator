28ca846197530c666ac4dd8b710c7db17b137940
Julien Richard-Foy <julien@richard-foy.fr>
2024-03-11 13:15:37 +0100

    Comment some lines

--- tests/src/main/scala/com/scylladb/migrator/Test.scala
+++ tests/src/main/scala/com/scylladb/migrator/Test.scala
@@ -14,11 +14,11 @@ object Test {
   def main(args: Array[String]): Unit = {
     scala.sys.process.Process("curl http://localhost:8001").run().exitValue()
 
-    val sourceDDb: AmazonDynamoDB = AmazonDynamoDBClientBuilder
-      .standard()
-      .withEndpointConfiguration(new EndpointConfiguration("http://localhost:8001", "eu-central-1"))
-      .build()
-    println(sourceDDb.listTables())
+//    val sourceDDb: AmazonDynamoDB = AmazonDynamoDBClientBuilder
+//      .standard()
+//      .withEndpointConfiguration(new EndpointConfiguration("http://localhost:8001", "eu-central-1"))
+//      .build()
+//    println(sourceDDb.listTables())
 
 
 //    println(targetAlternator.listTables())
