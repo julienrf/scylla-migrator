69dc45beaeb7f9443a9839012aa902e5e6d7e887
Julien Richard-Foy <julien@richard-foy.fr>
2024-03-11 13:32:28 +0100

    variation

--- tests/src/main/scala/com/scylladb/migrator/Test.scala
+++ tests/src/main/scala/com/scylladb/migrator/Test.scala
@@ -16,12 +16,12 @@ object Test {
     scala.sys.process.Process("curl http://localhost:8001").run().exitValue()
 
     Thread.sleep(2000)
-    val sourceDDb: AmazonDynamoDB = AmazonDynamoDBClientBuilder
-      .standard()
-      .withEndpointConfiguration(new EndpointConfiguration("http://localhost:8001", "eu-central-1"))
+//    val sourceDDb: AmazonDynamoDB = AmazonDynamoDBClientBuilder
+//      .standard()
+//      .withEndpointConfiguration(new EndpointConfiguration("http://localhost:8001", "eu-central-1"))
 //      .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("dummy", "dummy")))
-      .build()
-    println(sourceDDb.listTables())
+//      .build()
+//    println(sourceDDb.listTables())
 
 
 //    println(targetAlternator.listTables())
