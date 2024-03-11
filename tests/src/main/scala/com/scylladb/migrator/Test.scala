6d40f0e5625663d874ad6c30c125d91eb62ce2ab
Julien Richard-Foy <julien@richard-foy.fr>
2024-03-11 13:07:52 +0100

    More specific test

--- tests/src/main/scala/com/scylladb/migrator/Test.scala
+++ tests/src/main/scala/com/scylladb/migrator/Test.scala
@@ -5,20 +5,20 @@ import com.amazonaws.services.dynamodbv2.{AmazonDynamoDB, AmazonDynamoDBClientBu
 
 object Test {
 
-  val sourceDDb: AmazonDynamoDB = AmazonDynamoDBClientBuilder
-    .standard()
-    .withEndpointConfiguration(new EndpointConfiguration("http://localhost:8001", "eu-central-1"))
-    .build()
-
-  /** Client of a target Alternator instance */
-  val targetAlternator: AmazonDynamoDB = AmazonDynamoDBClientBuilder
-    .standard()
-    .withEndpointConfiguration(new EndpointConfiguration("http://localhost:8000", "eu-central-1"))
-    .build()
+//  val sourceDDb: AmazonDynamoDB = AmazonDynamoDBClientBuilder
+//    .standard()
+//    .withEndpointConfiguration(new EndpointConfiguration("http://localhost:8001", "eu-central-1"))
+//    .build()
+//
+//  /** Client of a target Alternator instance */
+//  val targetAlternator: AmazonDynamoDB = AmazonDynamoDBClientBuilder
+//    .standard()
+//    .withEndpointConfiguration(new EndpointConfiguration("http://localhost:8000", "eu-central-1"))
+//    .build()
 
   def main(args: Array[String]): Unit = {
     scala.sys.process.Process("curl http://localhost:8001").run().exitValue()
-    println(sourceDDb.listTables())
-    println(targetAlternator.listTables())
+//    println(sourceDDb.listTables())
+//    println(targetAlternator.listTables())
   }
 }
